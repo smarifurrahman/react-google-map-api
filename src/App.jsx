e9f3602a63dfa082d346ddae74d1583d3f8fedb2
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-06 20:53:52 +0600

    mylocation file added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyLocation from './components/MyLocation/MyLocation'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <MyLocation></MyLocation>
+    </div>
+  )
+}
+
+export default App
